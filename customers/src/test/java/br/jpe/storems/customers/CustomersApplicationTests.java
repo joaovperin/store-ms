6e804129e2e3ccb9ae67fa0fffbef6e9518376c7
João Victor Perin <joaovperin@live.com>
2021-07-28 08:59:33 -0300

    feat/hello-world
    Created projects using start.spring.io.

---
+++ customers/src/test/java/br/jpe/storems/customers/CustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jpe.storems.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
